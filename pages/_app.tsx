d5fdb59b1d5299adae503197e259e5be3e05b2f6
bolub <abiol5202@gmail.com>
2022-08-06 15:43:00 +0100

    update font

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -3,13 +3,13 @@ import type { AppProps } from 'next/app';
 import { ChakraProvider } from '@chakra-ui/react';
 import { theme } from '../chakra/theme';
 
-import '@fontsource/Inter/300.css';
-import '@fontsource/Inter/400.css';
-import '@fontsource/Inter/500.css';
-import '@fontsource/Inter/600.css';
-import '@fontsource/Inter/700.css';
-import '@fontsource/Inter/800.css';
-import '@fontsource/Inter/900.css';
+import '@fontsource/inter/300.css';
+import '@fontsource/inter/400.css';
+import '@fontsource/inter/500.css';
+import '@fontsource/inter/600.css';
+import '@fontsource/inter/700.css';
+import '@fontsource/inter/800.css';
+import '@fontsource/inter/900.css';
 
 import { QueryClient, QueryClientProvider } from 'react-query';
 import { ReactQueryDevtools } from 'react-query/devtools';
