a66ce3c0c99ee0f33467f00d5f2da2fb38506efd
bolub <abiol5202@gmail.com>
2022-08-06 15:47:14 +0100

    update

--- API/index.ts
+++ API/index.ts
@@ -3,6 +3,6 @@ import axios from 'axios';
 export default axios.create({
   baseURL: 'https://api.airtable.com/v0/appJGUjzAkZfyM3Ji',
   headers: {
-    Authorization: `Bearer keyKv6YAKZICfhyeG`,
+    Authorization: `Bearer ${process.env.NEXT_PUBLIC_TOKEN}`,
   },
 });
